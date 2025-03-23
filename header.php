f43d42b8e8e462df9f9edaddb58adaf70dda5bcd
Fahad Aljahlan <fahadmaj@hotmail.com>
2025-03-23 07:05:23 -0700

    Updated header nav links to use .php versions after HTML to PHP conversion

--- header.php
+++ header.php
@@ -11,10 +11,11 @@ $isLoggedIn = isset($_SESSION['user_id']); // Will be true if logged in
     <nav>
         <ul class="nav-links">
             <li><a href="index.php">Home</a></li>
-            <li><a href="browse.html">Browse Skills</a></li>
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="create-listing.html">Create Listing</a></li>
-            <li><a href="show-listings.html">Show Listings</a></li>
+            <li><a href="browse.php">Browse Skills</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="create-listing.php">Create Listing</a></li>
+            <li><a href="show-listings.php">Show Listings</a></li>
+            <li><a href="discussion.php">Discussions</a></li>
         </ul>
         <div class="auth-links">
             <?php if ($isLoggedIn): ?>
@@ -23,7 +24,7 @@ $isLoggedIn = isset($_SESSION['user_id']); // Will be true if logged in
                 </a>
                 <a href="logout.php" class="nav-logout">Logout</a>
             <?php else: ?>
-                <a href="register.php" class="profile-icon">
+                <a href="login.php" class="profile-icon">
                     <img src="images/user.png" alt="Register/Login">
                 </a>
             <?php endif; ?>
